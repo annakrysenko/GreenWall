16c93ff57e303ad4e4c73d70eafbd0db55ab9c77
Anna Krysenko <krysenkoanna@gmail.com>
2023-08-12 01:51:02 +0300

    full

--- src/js/fullScreenScroll.js
+++ src/js/fullScreenScroll.js
@@ -1,3 +1,8 @@
+let menuLinks = document.querySelectorAll('.slide-link');
+let burgerLinks = document.querySelectorAll('.burger-slide-link');
+let wrapper = document.querySelector('.wrapper-main');
+let pagination = document.querySelector('.page__pagination');
+
 let pageSlider = new Swiper('.page', {
   wrapperClass: 'page__wrapper',
   slideClass: 'page__screen',
@@ -30,19 +35,25 @@ let pageSlider = new Swiper('.page', {
     draggable: true,
   },
   init: false,
-  on:{
+  on: {
     init: function () {
-        menuSlider()
+      menuSlider();
+      setScrollType()
+      // menuSliderBurger()
     },
-    slideChange: function () {    
-        menuSliderRemove()
-        menuLinks[pageSlider.realIndex].classList.add('_active');
-
+    slideChange: function () {
+      menuSliderRemove();
+      // menuSliderRemoveBurger()
+      menuLinks[pageSlider.realIndex].classList.add('_active');
+    },
+    resize: function () {
+      setScrollType()
     }
-  }
+  },
+
 });
 
-let menuLinks = document.querySelectorAll('.slide-link');
+
 
 function menuSlider() {
   if (menuLinks.length > 0) {
@@ -64,6 +75,54 @@ function menuSliderRemove() {
   if (menuLinkActive) {
     menuLinkActive.classList.remove('_active');
   }
+}
+
+// burger menu
+function menuSliderBurger() {
+  if (menuLinks.length > 0) {
+    menuLinks[pageSlider.realIndex].classList.add('_active');
+    for (let index = 0; index < menuLinks.length; index++) {
+      const menuLink = menuLinks[index];
+      menuLink.addEventListener('click', function (e) {
+        menuSliderRemove();
+        pageSlider.slideTo(index, 800);
+        menuLink.classList.add('_active');
+        e.preventDefault();
+      });
+    }
+  }
+}
+function menuSliderRemoveBurger() {
+  let menuLinkActive = document.querySelector('.slide-link._active');
+  if (menuLinkActive) {
+    menuLinkActive.classList.remove('_active');
+  }
+}
+// end burger menu
+
+function setScrollType() {
+  if (wrapper.classList.contains('_free')) {
+    wrapper.classList.remove('_free');
+    pagination.style.display = 'flex';
+
+    pageSlider.params.freeMode = false;
+  }
+  for (let index = 0; index < pageSlider.slides.length; index++) {
+    const pageSlide = pageSlider.slides[index];
+    const pageSlideContent = pageSlide.querySelector('.screen__content');
+    if (pageSlideContent) {
+      const pageSlideContentWidth = pageSlideContent.offsetWidth;
+      // console.log(pageSlideContentWidth)
+      if (pageSlideContentWidth < 780) {
+        // console.log('first')
+        wrapper.classList.add('_free');
+        pagination.style.display = 'none';
+        pageSlider.params.freeMode = true;
+        break;
+      }
+    }
+  }
+
 }
 
 pageSlider.init();
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,39 @@
 (() => {
   const refs = {
+    openModalHeroBtn: document.querySelector('.contact-us-btn-hero'),
+
+
     openModalBtn: document.querySelector('.contact-us-btn'),
     closeModalBtn: document.getElementById('close'),
     backdrop: document.querySelector('.backdrop'),
+
+    openModalBurgerBtn: document.querySelector('.icon-menu'),
+    closeModalBurgerBtn: document.getElementById('close-burger'),
+    modalBurger: document.querySelector('.burger-menu'),
+
+    burgerLinks :document.querySelectorAll('.burger-nav-list__item')
+
   };
 
-  if(refs.openModalBtn)refs.openModalBtn.addEventListener('click', toggleModal);
-  if( refs.closeModalBtn)refs.closeModalBtn.addEventListener('click', toggleModal);
-  if(refs.backdrop)refs.backdrop.addEventListener('click', e => onClickBackdrop(e));
+  if(refs.openModalHeroBtn)
+    refs.openModalHeroBtn.addEventListener('click', toggleModal);
+
+
+  if (refs.openModalBtn)
+    refs.openModalBtn.addEventListener('click', toggleModal);
+  if (refs.closeModalBtn)
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (refs.backdrop)
+    refs.backdrop.addEventListener('click', e => onClickBackdrop(e));
+
+  if (refs.closeModalBurgerBtn)
+    refs.closeModalBurgerBtn.addEventListener('click', toggleModalBurger);
+  if (refs.openModalBurgerBtn)
+    refs.openModalBurgerBtn.addEventListener('click', toggleModalBurger);
+
+  function toggleModalBurger() {
+    refs.modalBurger.classList.toggle('is-hidden');
+  }
 
   function toggleModal(e) {
     refs.backdrop.classList.toggle('is-hidden');
@@ -18,4 +44,17 @@
       toggleModal();
     }
   }
+
+
+refs.burgerLinks.forEach((link) => {
+    if(link){
+        link.addEventListener('click', () => {
+          toggleModalBurger()
+          const anchor = link.getAttribute('data-section');
+          console.log(anchor)
+          window.location.hash= anchor;
+        })
+    }
+})
+
 })();
